6dee60909af06bfc61d5823ae5f469b9f59ca2f5
Felix Moon <daekeun.moon@gmail.com>
2017-10-28 23:15:41 -0500

    Completed spotify-this-song function and movie-this function

--- keys.js
+++ keys.js
@@ -1,11 +1,20 @@
-console.log('this is loaded');
+console.log('[keys.js] is loaded...');
 
 var twitterKeys = {
-  consumer_key: '<input here>',
-  consumer_secret: '<input here>',
-  access_token_key: '<input here>',
-  access_token_secret: '<input here>',
+	consumer_key: 'oTUgdjuYqENgGLiHl5tGOG6Bs',
+	consumer_secret: '6ogrTtUQsOfIAakj2cu67JP0BbSSnPCf5qnqbGluAfbzZUp6xJ',
+	access_token_key: '924147073238396928-ydaImgATYr7ttN2ZTyvTdfkS6ZtGrz3',
+	access_token_secret: '8hkU4IkN6S0i3kveuQUzxh56qDbkV2VASRSz0QF0Ff1NX',
 }
 
-module.exports = twitterKeys;
+var spotifyKeys = {
+	id: '023f162e5c7a40b0a12027f92a31ee6e',
+	secret: '5710b1c01fcb41d49eb6517413dd66b2'
+}
+
+module.exports = {
+	twitterKeys: twitterKeys,
+	spotifyKeys: spotifyKeys,
+	omdbapiKey: '40e9cece'
+};
 
---
+++ liri.js
@@ -0,0 +1,138 @@
+var importKeys = require("./keys.js");
+var Twitter = require('twitter');
+var Spotify = require('node-spotify-api');
+var request = require('request');
+var fs = require('fs');
+
+var command = process.argv[2];
+
+switch(command) {
+	case "my-tweets":
+		myTweets();
+		break;
+
+	case "spotify-this-song":
+		spotifyThisSong();
+		break;
+
+	case "movie-this":
+		movieThis();
+		break;
+
+	case "do-what-it-says":
+		doWhatItSays();
+		break;
+
+	default:
+		console.log("Invalid command! Come again with one of [my-tweets, spotify-this-song, movie-this, do-what-it-says]");
+		break;
+}
+
+function myTweets() {
+	var client = new Twitter({
+		consumer_key: importKeys.twitterKeys.consumer_key,
+		consumer_secret: importKeys.twitterKeys.consumer_secret,
+		access_token_key: importKeys.twitterKeys.access_token_key,
+		access_token_secret: importKeys.twitterKeys.access_token_secret
+	});
+
+	var params = {screen_name: 'nodejs'};
+
+	client.get('statuses/user_timeline', params, function(error, tweets, response) {
+		if (error) {
+			return console.log('Error occurred: ' + error);
+		}
+
+		console.log(tweets);
+		// console.log(response);
+	});	
+}
+
+function spotifyThisSong() {
+	var songName = process.argv[3];
+
+	for(var i=4; i<process.argv.length; i++) {
+		songName += (" " + process.argv[i]);
+	}
+	console.log("Finding <" + songName + ">...");
+
+	if(songName === undefined) {
+		console.log("You should add a song name!");
+		return;
+	}
+
+	var spotify = new Spotify({
+		id: importKeys.spotifyKeys.id,
+		secret: importKeys.spotifyKeys.secret
+	});	
+
+	spotify.search({ type: 'track', query: songName, limit: 20 }, function(err, data) {
+		if (err) {
+			return console.log('Error occurred: ' + err);
+		}
+
+		// console.log(JSON.stringify(data, null, 4)); 
+
+		var foundSong = data.tracks.items;
+
+		console.log("\n");
+
+		for(var j=0; j<foundSong.length; j++) {
+			if((j+1) < 10) {
+				console.log("=[0" + (j+1) + "]========================================================");
+			}
+			else {
+				console.log("=[" + (j+1) + "]========================================================");
+			}
+			console.log("* Artist(s): " + foundSong[j].artists[0].name);
+			console.log("* The song's name: " + foundSong[j].name);
+			console.log("* A preview link: " + foundSong[j].preview_url);
+			console.log("* The album: " + foundSong[j].album.name);
+			console.log("=============================================================\n");
+		}
+	});
+}
+
+function movieThis() {
+	var movieName = process.argv[3];
+
+	for(var i=4; i<process.argv.length; i++) {
+		movieName += ("+" + process.argv[i]);
+	}
+	console.log("Finding <" + movieName + ">...");
+
+	if(movieName === undefined) {
+		console.log("You should add a movie name!");
+		return;
+	}
+
+	var queryURL = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=" + importKeys.omdbapiKey;
+
+	request(queryURL, function (error, response, body) {
+		// If the request is successful (i.e. if the response status code is 200)
+		if (!error && response.statusCode === 200) {
+			var foundMovie = JSON.parse(body);
+
+			// console.log(JSON.stringify(foundMovie, null, 4));
+
+			console.log("\n=============================================================");
+			console.log("* Title: " + foundMovie.Title);
+			console.log("* Year: " + foundMovie.Year);
+			console.log("* IMDB Rating: " + foundMovie.Ratings[0].Value);
+			console.log("* Rotten Tomatoes Rating: " + foundMovie.Ratings[1].Value);
+			console.log("* Country: " + foundMovie.Country);
+			console.log("* Language: " + foundMovie.Language);
+			console.log("* Plot: " + foundMovie.Plot);
+			console.log("* Actors: " + foundMovie.Actors);			
+			console.log("=============================================================\n");
+		}
+		else {
+			console.log('Error occurred: ' + error);
+		}
+	});
+}
+
+function doWhatItSays() {
+	// fs.read
+}
+
